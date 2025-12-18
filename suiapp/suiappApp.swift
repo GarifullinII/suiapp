60e591532f0052db1d6a927893bbc695067fac22
GarifullinII <ildaridrisovich@gmail.com>
2025-12-18 03:17:53 -0800

    Initial Commit

---
+++ suiapp/suiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiappApp.swift
+//  suiapp
+//
+//  Created by Ildar Garifullin on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct suiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
